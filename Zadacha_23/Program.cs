02693af367b0568ae12be38d08f732e49e324745
Diana Naumenko <diananaymenko@mail.ru>
2023-04-23 22:09:06 +0300

    Zadacha_23

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125;
+
+Console.Write("Введите число N: ");
+string text_N = Console.ReadLine();
+double N =Convert.ToInt32(text_N);
+double x = 0;
+double y = 0;
+while (x <= N)
+{
+    y = Math.Pow(x, 3);
+    Console.WriteLine(y);
+    x++;
+}
